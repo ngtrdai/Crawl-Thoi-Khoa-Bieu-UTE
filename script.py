9d3e7ba16d878fdc73cd82265f0485e00d122842
ngtrdai <ngtrdai@svute.com>
2021-07-08 09:35:29 +0700

    UPDATE

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 # Import thư viện
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from time import sleep
 import json
